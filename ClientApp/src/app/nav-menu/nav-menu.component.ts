002945b3e34834816fd48571cf03c534a5ec13fe
Joakim <joakim@kvalifik.dk>
2020-04-27 15:32:29 +0200

    Fix tabs
    Co-authored-by: Tim Jensen <s185027@student.dtu.dk>
    Co-authored-by: ida98charlotte <s185041@student.dtu.dk>
    Co-authored-by: Cecilie Drejer <s185032@student.dtu.dk>

--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-menu',
@@ -8,10 +9,13 @@ import { Component } from '@angular/core';
 export class NavMenuComponent {
   isExpanded = false;
   navLinks = [
-    {path: "/", label: "Dashboard"},
-    {path: "/my-characters", label: "My Characters"},
-    {path: "/", label: "Dashboard"}
+    {path: "/", label: "Dashboard", index: 0},
+    {path: "/my-campaigns", label: "My Campaigns", index: 1},
+    {path: "/my-characters", label: "My Characters", index: 2}
   ]
+  activeLinkIndex = -1
+
+  constructor(private router: Router) {}
 
   collapse() {
     this.isExpanded = false;
@@ -19,5 +23,11 @@ export class NavMenuComponent {
 
   toggle() {
     this.isExpanded = !this.isExpanded;
+  }
+  
+  ngOnInit(): void {
+    this.router.events.subscribe((res) => {
+        this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.path === '.' + this.router.url));
+    });
   }
 }
