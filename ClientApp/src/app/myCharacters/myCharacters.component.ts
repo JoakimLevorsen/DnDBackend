db8b525fc17db7f7157f74253caef5a614ba4261
Ida Christensen <idac1998@gmail.com>
2020-05-04 15:10:19 +0200

    Cleaned up a bit

--- ClientApp/src/app/myCharacters/myCharacters.component.ts
+++ ClientApp/src/app/myCharacters/myCharacters.component.ts
@@ -1,26 +1,9 @@
-import { Component, Inject } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "MyCharacters",
     templateUrl: "./myCharacters.component.html",
 })
 export class MyCharactersComponent {
-    public forecasts: WeatherForecast[];
-
-    constructor(http: HttpClient, @Inject("BASE_URL") baseUrl: string) {
-        http.get<WeatherForecast[]>(baseUrl + "weatherforecast").subscribe(
-            result => {
-                this.forecasts = result;
-            },
-            error => console.error(error)
-        );
-    }
-}
-
-interface WeatherForecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
+    constructor() {}
 }
