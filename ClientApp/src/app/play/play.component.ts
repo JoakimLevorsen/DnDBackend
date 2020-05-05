07620b99cf97884c477dff0c358a09182c9fcbc7
Ida Christensen <idac1998@gmail.com>
2020-05-05 18:49:42 +0200

    Play design

--- ClientApp/src/app/play/play.component.ts
+++ ClientApp/src/app/play/play.component.ts
@@ -1,7 +1,8 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: "play",
-    templateUrl: "./play.component.html",
+    selector: 'play',
+    templateUrl: './play.component.html',
+    styleUrls: ['./play.component.css'],
 })
 export class PlayComponent {}
