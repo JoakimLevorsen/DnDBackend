bf822f6fd18f898f860d1d56b7118d889931a550
TMJ97 <s185027@student.dtu.dk>
2020-05-05 11:25:51 +0200

    Dashboard Dialog is perfect

--- ClientApp/src/app/dashboard/dashboard.component.ts
+++ ClientApp/src/app/dashboard/dashboard.component.ts
@@ -1,22 +1,22 @@
-import { Component, Inject } from "@angular/core";
+import { Component, Inject } from '@angular/core';
 import {
     MatDialog,
     MAT_DIALOG_DATA,
     MatDialogConfig,
     MatDialogRef,
-} from "@angular/material/dialog";
+} from '@angular/material/dialog';
 
 export interface DialogData {
-    joinByIDNumber: number;
+    joinByIDNumber: string;
     joinByIDPassword: string;
 }
 @Component({
-    selector: "dashboard",
-    templateUrl: "./dashboard.component.html",
-    styleUrls: ["./dashboard.component.css"],
+    selector: 'dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-    joinByIDNumber: number;
+    joinByIDNumber: string;
     joinByIDPassword: string;
 
     constructor(public dialog: MatDialog) {}
@@ -25,7 +25,7 @@ export class DashboardComponent {
         const dialogConfig = new MatDialogConfig();
         dialogConfig.disableClose = true;
         dialogConfig.autoFocus = true;
-        dialogConfig.width = "250px";
+        dialogConfig.width = '250px';
 
         const dialogRef = this.dialog.open(
             DashboardComponentDialog,
@@ -33,16 +33,24 @@ export class DashboardComponent {
         );
 
         dialogRef.afterClosed().subscribe(result => {
-            console.log("The dialog was closed");
-            console.log(this.joinByIDNumber);
-            console.log(this.joinByIDPassword);
+            console.log('The dialog was closed');
+            console.log(result);
+            this.joinByIDNumber = result[0];
+            this.joinByIDPassword = result[1];
+            if (result != undefined) {
+                //TODO Call function to join a campaign
+            } else {
+                //Do nothing
+            }
         });
     }
+
+    //TODO Make function that actually uses ID and Pass to join a campaign
 }
 
 @Component({
-    selector: "dashboard-dialog",
-    templateUrl: "./dashboard.component.dialog.html",
+    selector: 'dashboard-dialog',
+    templateUrl: './dashboard.component.dialog.html',
 })
 export class DashboardComponentDialog {
     constructor(
