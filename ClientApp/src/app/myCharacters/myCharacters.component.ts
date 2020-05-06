13a65c814f3503a481e2b3a2ee1a6b8afa9e5635
Joakim <joakim@kvalifik.dk>
2020-05-06 14:22:19 +0200

    nice2look@

--- ClientApp/src/app/myCharacters/myCharacters.component.ts
+++ ClientApp/src/app/myCharacters/myCharacters.component.ts
@@ -4,16 +4,21 @@ import { GameState } from 'src/websocket/responses/GameState';
 import { WebSocketService } from 'src/websocket';
 
 class Character {
+    public name: string;
+    public ID: number;
+    public level: number;
+    public health: number;
+    public xp: number;
+    public cRace: string;
+    public cClass: string;
+    public campaignName?: string;
     constructor(
-        public name: string,
-        public ID: number,
-        public level: number,
-        public health: number,
-        public xp: number,
-        public cRace: string,
-        public cClass: string,
-        public campaignName?: string
-    ) {}
+        character: GameState['characters'][number],
+        campaignName: string
+    ) {
+        Object.assign(this, character);
+        this.campaignName = campaignName;
+    }
 }
 
 @Component({
@@ -23,30 +28,24 @@ class Character {
 })
 export class MyCharactersComponent implements OnInit {
     panelOpenState = false;
-    myCharactersFromGameState: GameState['characters'];
     myCharacters: Character[];
 
     constructor(private router: Router, private socket: WebSocketService) {}
 
     ngOnInit() {
         this.socket.gameState$.subscribe(s => {
-            this.myCharactersFromGameState =
-                s?.characters?.filter(c => c.owner === s.me) ?? [];
-            for (let character of this.myCharactersFromGameState) {
-                const characterToShow = new Character(
-                    character.name,
-                    character.ID,
-                    character.level,
-                    character.health,
-                    character.xp,
-                    character.cRace,
-                    character.cClass,
-                    s?.joinedCampaigns.find(
-                        c => c.ID === character.campaign
-                    )?.name
-                );
-                this.myCharacters.push(characterToShow);
-            }
+            this.myCharacters =
+                s?.characters
+                    ?.filter(c => c.owner === s.me)
+                    .map(
+                        character =>
+                            new Character(
+                                character,
+                                s?.joinedCampaigns.find(
+                                    c => c.ID === character.campaign
+                                )?.name
+                            )
+                    ) ?? [];
             console.log('myCharacters:', this.myCharacters); // TODO: undefined - gør måske noget med databasen forkert
         });
     }
