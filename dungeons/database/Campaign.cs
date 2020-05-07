65e50f83826caf74bbf572d8508ef1d81f4f8d25
Joakim Levorsen <joakim.levorsen@icloud.com>
2020-05-07 13:52:58 +0200

    Players array for Campaigns

--- dungeons/database/Campaign.cs
+++ dungeons/database/Campaign.cs
@@ -1,8 +1,14 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace dungeons.database {
-    public class Campaign {
+namespace dungeons.database
+{
+    public class Campaign
+    {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
         public string name { get; set; }
@@ -12,7 +18,14 @@ namespace dungeons.database {
         public int maxPlayers { get; set; }
         public string? password { get; set; }
         public DateTime modificationDate { get; set; }
-        
+
         public User dungeonMaster { get; set; }
+        public async Task<List<Character>> players()
+        {
+            using (var context = new GameContext())
+            {
+                return await context.characters.Where(c => c.campaign != null && c.campaign.ID == this.ID).ToListAsync();
+            }
+        }
     }
 }
