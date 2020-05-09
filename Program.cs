5597bb51bbd738792b099d46e65cd35d897a0596
TMJ97 <s185027@student.dtu.dk>
2020-05-09 12:14:40 +0200

    Random port testing stuff

--- Program.cs
+++ Program.cs
@@ -22,11 +22,7 @@ namespace D_D_Backend
                 {
                     webBuilder
                     .UseUrls(
-                        "http://localhost:5000",
-                        "http://localhost:5001",
-                        "http://130.225.170.238:5000",
-                        "http://130.225.170.238:5001",
-                        "http://130.225.170.238:80"
+                        "http://*:80"
                     )
                     .UseStartup<Startup>();
                 });
